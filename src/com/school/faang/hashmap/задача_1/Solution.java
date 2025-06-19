3dea0c29a0d64d007278fbdafb9b255257d06619
gellivar <gellivar@mail.ru>
2025-06-20 00:20:15 +0300

    Решение задачи 1.

--- src/com/school/faang/hashmap/задача_1/Solution.java
+++ src/com/school/faang/hashmap/задача_1/Solution.java
@@ -1,7 +1,39 @@
 package com.school.faang.hashmap.задача_1;
 
-public class Solution {
-    public static void main(String[] args) {
+import java.util.HashMap;
 
+public class Solution {
+    private HashMap<String, Integer> likesMap;
+
+    public Solution() {
+        likesMap = new HashMap<>();
+    }
+
+    public void likeVideo(String videoId) {
+        if (!likesMap.containsKey(videoId)) {
+            likesMap.put(videoId, 1);
+        }
+        else {
+            likesMap.put(videoId, likesMap.get(videoId) + 1);
+        }
+    }
+
+    public int getLikes(String videoId) {
+        if (likesMap.containsKey(videoId)) {
+            return likesMap.get(videoId);
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.likeVideo("dQw4w9WgXcQ");
+        solution.likeVideo("dQw4w9WgXcQ");
+        solution.likeVideo("asd32ad");
+        System.out.println(solution.getLikes("dQw4w9WgXcQ"));
+        System.out.println(solution.getLikes("asd32ad"));
+        System.out.println(solution.getLikes("daqeqw2sa3AS"));
     }
 }
