c8b5fd731a04261f0f584f5130e4397a710fe716
Viktor Gavrilov <Gavr320@yandex.ru>
2025-06-19 22:24:32 +0300

    Решение задачи 1

--- src/com/school/faang/hashmap/задача_1/Solution.java
+++ src/com/school/faang/hashmap/задача_1/Solution.java
@@ -1,7 +1,20 @@
 package com.school.faang.hashmap.задача_1;
 
+import java.util.HashMap;
+
 public class Solution {
+
+    private final HashMap<String, Integer> likes = new HashMap<>();
+
     public static void main(String[] args) {
 
+    }
+
+    public void likeVideo(String videoId) {
+        likes.put(videoId, likes.getOrDefault(videoId, 0) + 1);
+    }
+
+    public int getLikes(String videoId) {
+        return likes.getOrDefault(videoId, 0);
     }
 }
