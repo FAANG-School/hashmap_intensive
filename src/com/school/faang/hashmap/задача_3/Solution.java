534e317de3e8f7dfbcffbf0f66c0a6ff0e90dbeb
Дядька <mr.kotman@list.ru>
2025-06-20 01:10:07 +0300

    Реализовал решение задачи 2 Cоздал отдельный класс Manager, который управляет действиями.

--- src/com/school/faang/hashmap/задача_3/Solution.java
+++ src/com/school/faang/hashmap/задача_3/Solution.java
@@ -2,6 +2,7 @@ package com.school.faang.hashmap.задача_3;
 
 public class Solution {
     public static void main(String[] args) {
-
+        Product product = new Product("120AQ","Наручники");
+        Product product2 = new Product("120AQ","Наручники");
     }
 }
