853543f5f38d320c1faed4a3cef65098563bcc00
240103158 <omirzak431@gmail.com>
2025-06-20 15:08:41 +0500

    Task 1

--- src/com/school/faang/hashmap/задача_1/Solution.java
+++ src/com/school/faang/hashmap/задача_1/Solution.java
@@ -25,6 +25,7 @@ public class Solution {
     static void likeVideo(String videoId){
         if(!counter.containsKey(videoId)){
             counter.put(videoId, 1);
+            System.out.println(counter.get(videoId));
         }else {
             counter.put(videoId, counter.get(videoId) + 1);
         }
