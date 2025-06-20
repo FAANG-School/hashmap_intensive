0556a7bf1c979ed214b3340dda5171865803d6a3
egormilov <eg.damarad@gmail.com>
2025-06-20 23:54:10 +0300

    Решение задачи 3

---
+++ src/com/school/faang/hashmap/задача_3/Product.java
@@ -0,0 +1,36 @@
+package com.school.faang.hashmap.задача_3;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Product {
+    private String productId;
+    private String name;
+
+    public Product(String productId, String name) {
+        this.productId = productId;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(productId, product.productId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId='" + productId + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
--- src/com/school/faang/hashmap/задача_3/Solution.java
+++ src/com/school/faang/hashmap/задача_3/Solution.java
@@ -1,7 +1,21 @@
 package com.school.faang.hashmap.задача_3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public static void main(String[] args) {
 
+        Map<Product, Integer> basket = new HashMap<>();
+
+        Product product1 = new Product("NK-42-BK", "Фен");
+        Product product2 = new Product("NK-42-BK", "Фен");
+
+        basket.put(product1, 1);
+        basket.put(product2, basket.getOrDefault(product1, 0) + 1);
+
+        for (Map.Entry<Product, Integer> entry : basket.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
     }
 }
