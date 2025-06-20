5189141db0ccb4f93f456ed13e1d947e65eea74d
isavelyev76 <154694236+isavelyev76@users.noreply.github.com>
2025-06-20 15:34:01 +0300

    Решение задачи 3

--- src/com/school/faang/hashmap/задача_3/Solution.java
+++ src/com/school/faang/hashmap/задача_3/Solution.java
@@ -1,7 +1,24 @@
 package com.school.faang.hashmap.задача_3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+    private static final Map<Product, Integer> basket = new HashMap<>();
+
+
     public static void main(String[] args) {
+        Product product1 = new Product("X12345", "Product Test");
+        Product product2 = new Product("X12345", "Product Test");
+
+        addProduct(product1, 2);
+        addProduct(product2, 4);
+
+        System.out.println(basket);
+    }
+
+    static void addProduct(Product product, Integer amount) {
+        basket.put(product, basket.getOrDefault(product, 0) + amount);
 
     }
 }
