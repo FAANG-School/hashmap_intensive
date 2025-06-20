37d738ceb6d281038c3842cf4e0e0ea88652bfb3
Segalas <hudos@rambler.ru>
2025-06-20 22:11:12 +0300

    add lesson 6

--- src/com/school/faang/hashmap/задача_6/Solution.java
+++ src/com/school/faang/hashmap/задача_6/Solution.java
@@ -1,7 +1,19 @@
 package com.school.faang.hashmap.задача_6;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Solution {
     public static void main(String[] args) {
+        TweetService tweetService = new TweetService();
+        tweetService.createOrPutTweet("Alice", "java", "spring");
+        tweetService.createOrPutTweet("Bob", "java", "docker");
+        tweetService.createOrPutTweet("Alice", "java", "spring");
+        tweetService.createOrPutTweet("Charlie", "docker");
+
+        Map<String, String> topAuthors = tweetService.findTopAuthors();
+        System.out.println(topAuthors);
 
     }
 }
