a501b900a430e20ccc639dfb7558313245d2a662
VladTca <votsats@gmail.com>
2025-06-21 11:27:15 +0200

    Решение задачи 2

--- src/com/school/faang/hashmap/задача_2/Solution.java
+++ src/com/school/faang/hashmap/задача_2/Solution.java
@@ -1,7 +1,26 @@
 package com.school.faang.hashmap.задача_2;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+
 public class Solution {
+    Map<String,User> map = new HashMap<>();
+    
+    public void addUser(String string,User user){
+        map.put(string, user);
+    }
     public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.addUser("@john_doe", new User(1L, "John Doe", "2024-01-15"));
+        solution.addUser("@jane_smith", new User(2L, "Jane Smith", "2024-02-20"));
+        solution.addUser("@bob_wilson", new User(3L, "Bob Wilson", "2024-03-10"));
+
+        for (Map.Entry<String, User> entry : solution.map.entrySet()) {
+            System.out.println("Пользователь " + entry.getKey() + ", полное имя: " + entry.getValue().fullName);
+        }
 
     }
 }
---
+++ src/com/school/faang/hashmap/задача_2/User.java
@@ -0,0 +1,13 @@
+package com.school.faang.hashmap.задача_2;
+
+public class User {
+    long userId;
+    String fullName;
+    String registrationDate;
+
+    public User(long userId, String fullName, String registrationDate) {
+        this.userId = userId;
+        this.fullName = fullName;
+        this.registrationDate = registrationDate;
+    }
+}
