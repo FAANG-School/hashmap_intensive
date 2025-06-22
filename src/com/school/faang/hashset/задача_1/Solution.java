26a571d896927c84be562f294a3fcfc738388f3c
Ekaterina Ramakoti <zubkova-92@mail.ru>
2025-06-23 00:07:31 +0300

    Решение задачи 1

--- src/com/school/faang/hashset/задача_1/Solution.java
+++ src/com/school/faang/hashset/задача_1/Solution.java
@@ -2,6 +2,18 @@ package com.school.faang.hashset.задача_1;
 
 public class Solution {
     public static void main(String[] args) {
+        EventRegistration registration = new EventRegistration();
 
+        registration.register("alice@example.com");
+        registration.register("bob@example.com");
+        registration.register("charlie@example.com");
+
+        registration.register("bob@example.com");
+
+        System.out.println("Итоговое количество участников: " + registration.getParticipantCount());
+
+        String emailToCheck = "alice@example.com";
+        System.out.println("Пользователь " + emailToCheck + " зарегистрирован: " +
+                registration.isRegistered(emailToCheck));
     }
 }
