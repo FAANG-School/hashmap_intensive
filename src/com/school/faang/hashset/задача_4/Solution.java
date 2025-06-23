bcb563005148b7fb0720beb1687025d1f9b2215e
isavelyev76 <154694236+isavelyev76@users.noreply.github.com>
2025-06-23 21:31:52 +0300

    Решение задачи 4 hashset

--- src/com/school/faang/hashset/задача_4/Solution.java
+++ src/com/school/faang/hashset/задача_4/Solution.java
@@ -2,6 +2,24 @@ package com.school.faang.hashset.задача_4;
 
 public class Solution {
     public static void main(String[] args) {
+        ChannelManager channelManager = new ChannelManager();
+
+        channelManager.addUser("id123", "админ");
+        channelManager.addUser("id456", "модератор");
+        channelManager.addUser("id789", "участник");
+
+        System.out.println();
+
+        System.out.println("Статус наличия участника id456 в группе: " +
+                channelManager.hasUser("id456"));
+        System.out.println("Статус наличия участника id457 в группе: " +
+                channelManager.hasUser("id457"));
+
+        System.out.println();
+
+        System.out.println("user id123: " + channelManager.getRole("id123"));
+        channelManager.removeUser("id123");
+        System.out.println("user id123: " + channelManager.getRole("id123"));
 
     }
 }
