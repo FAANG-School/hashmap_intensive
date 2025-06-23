96256b38a7d99acb9960b3d379e60688b2f8af8a
Daniil Lepetyuha <daniil.lepetyuha@yandex.ru>
2025-06-23 14:14:15 +0300

    Решение задачи 1

--- src/com/school/faang/hashset/задача_1/Solution.java
+++ src/com/school/faang/hashset/задача_1/Solution.java
@@ -2,6 +2,16 @@ package com.school.faang.hashset.задача_1;
 
 public class Solution {
     public static void main(String[] args) {
+        EventRegistration eventRegistration = new EventRegistration();
 
+        eventRegistration.register("@people1");
+        eventRegistration.register("@people2");
+        eventRegistration.register("@people3");
+        eventRegistration.register("@people4");
+        eventRegistration.register("@people5");
+        eventRegistration.register("@people3");
+
+        System.out.println(eventRegistration.getParticipantCount());
+        System.out.println(eventRegistration.isRegistered("@people2"));
     }
 }
