d0d9e476f132ee2ed62e2d4db32dbad0d08ce425
Дядька <mr.kotman@list.ru>
2025-06-23 23:42:44 +0300

    Реализовал решение 6 задачи (HashSet)

--- src/com/school/faang/hashset/задача_6/Solution.java
+++ src/com/school/faang/hashset/задача_6/Solution.java
@@ -1,7 +1,24 @@
 package com.school.faang.hashset.задача_6;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Solution {
     public static void main(String[] args) {
+        HashSet<String> subscriptions = new HashSet<>();
+        HashSet<String> watchHistory = new HashSet<>();
+        watchHistory.add("Ёжик в тумане");
 
+        RecommendationEngine recomend = new RecommendationEngine();
+
+        recomend.addVideo("1","Ёжик в тумане");
+        recomend.addVideo("2","Ёж 2. Новые иголки");
+        recomend.addVideo("3","Просто Ёж и точка");
+        recomend.addVideo("4","Влад, что всё таки собрать на Войда?");
+
+        List<String> recommendationsVideos= recomend.generateFeed(subscriptions, watchHistory);
+        System.out.println(recommendationsVideos);
     }
 }
