c6dbd0cb8bad9cea53c80606b046655b2e9a0120
Daniil Lepetyuha <daniil.lepetyuha@yandex.ru>
2025-06-23 14:45:32 +0300

    Решение задачи 2

--- src/com/school/faang/hashset/задача_2/Solution.java
+++ src/com/school/faang/hashset/задача_2/Solution.java
@@ -2,6 +2,22 @@ package com.school.faang.hashset.задача_2;
 
 public class Solution {
     public static void main(String[] args) {
+        printInfo();
+    }
 
+    private static void printInfo() {
+        UserBlacklist userBlacklist = new UserBlacklist();
+
+        userBlacklist.blockUser("@people1");
+        userBlacklist.blockUser("@people2");
+        userBlacklist.blockUser("@people3");
+
+        userBlacklist.unblockUser("@people1");
+        userBlacklist.unblockUser("@people2");
+        userBlacklist.unblockUser("@people3");
+
+        System.out.println(userBlacklist.isUserBlocked("@people1"));
+        System.out.println(userBlacklist.isUserBlocked("@people2"));
+        System.out.println(userBlacklist.isUserBlocked("@people3"));
     }
 }
