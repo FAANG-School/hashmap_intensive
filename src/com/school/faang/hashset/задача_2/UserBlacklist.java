0e988bb8bf24481aed01d8e28d0988ee6ec910ef
Kirill <d5a.d5a@yandex.ru>
2025-06-23 08:46:28 +0300

    HashSet Task2

--- src/com/school/faang/hashset/задача_2/UserBlacklist.java
+++ src/com/school/faang/hashset/задача_2/UserBlacklist.java
@@ -7,25 +7,30 @@ public class UserBlacklist {
     private HashSet<String> blacklist = new HashSet<>();
 
     void blockUser(String username) {
-        // Добавляет пользователя в ЧС
+
         blacklist.add(username);
+
     }
 
     void unblockUser(String username) {
-        // Удаляет пользователя из ЧС
+
         blacklist.remove(username);
+
     }
 
     boolean isUserBlocked(String username) {
-        // Проверяет заблокирован ли пользователь
+
         return blacklist.contains(username);
+
     }
 
 
     @Override
     public String toString() {
+
         return "UserBlacklist{" +
                 "blacklist=" + blacklist +
                 '}';
+
     }
 }
