a9e52f185c959f205f8c4c5d84a2f89f4f133540
Daniil Lepetyuha <daniil.lepetyuha@yandex.ru>
2025-06-24 16:12:27 +0300

    Решение задачи - 4

--- src/com/school/faang/hashset/задача_4/Solution.java
+++ src/com/school/faang/hashset/задача_4/Solution.java
@@ -1,7 +1,37 @@
 package com.school.faang.hashset.задача_4;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Solution {
     public static void main(String[] args) {
+        printInfo();
+    }
 
+    private static void printInfo() {
+        HashMap<String, String> memberRoles = new HashMap<>();
+
+        HashSet<String> members = new HashSet<>();
+
+        ChannelManager manager = new ChannelManager(members, memberRoles);
+
+        manager.addUser("1", "админ");
+        manager.addUser("2", "модератор");
+        manager.addUser("3", "участник");
+        manager.addUser("4", "участник");
+        manager.addUser("5", "модератор");
+        manager.addUser("6", "участник");
+        manager.addUser("7", "участник");
+
+        System.out.println(manager.hasUser("3"));
+        System.out.println(manager.hasUser("7"));
+        System.out.println(manager.hasUser("9"));
+
+        System.out.println(manager.getRole("1"));
+        System.out.println(manager.getRole("9"));
+
+        manager.removeUser("5");
+        System.out.println(manager.hasUser("5"));
+        System.out.println(manager.getRole("5"));
     }
 }
