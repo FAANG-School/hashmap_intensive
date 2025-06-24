170bd9b921a0068453a5d72c526218db33e2dfd9
quikkkk <kexitttttt@gmail.com>
2025-06-24 16:15:35 +0300

    Решение задачи - 1

--- src/com/school/faang/hashset/задача_1/Solution.java
+++ src/com/school/faang/hashset/задача_1/Solution.java
@@ -1,5 +1,6 @@
 package com.school.faang.hashset.задача_1;
 
+
 public class Solution {
     public static void main(String[] args) {
         EventRegistration registration = new EventRegistration();
