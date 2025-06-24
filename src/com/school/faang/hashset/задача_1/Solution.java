bbfc2cceb5cd5412936901c425e7bb807eeb3af2
MichsSS-G <mich.gribakin.fml@yandex.ru>
2025-06-24 20:02:56 +0500

    Added solution to task 1

--- src/com/school/faang/hashset/задача_1/Solution.java
+++ src/com/school/faang/hashset/задача_1/Solution.java
@@ -2,6 +2,10 @@ package com.school.faang.hashset.задача_1;
 
 public class Solution {
     public static void main(String[] args) {
-
+        EventRegistrationManager eventRegistrationManager = new EventRegistrationManager();
+        eventRegistrationManager.addRegistration("1223@gmail.com");
+        eventRegistrationManager.addRegistration("wow@yandex.ru");
+        System.out.println(eventRegistrationManager.isRegistered("1223@gmail.com"));
+//        eventRegistrationManager.addRegistration("wow@yandex.ru");
     }
 }
