9b54322692a8d247200e8220c8810dfa86740a5c
QuiK000 <57329302+QuiK000@users.noreply.github.com>
2025-06-24 16:21:32 +0300

    Решение задачи - 6

--- src/com/school/faang/hashset/задача_6/Solution.java
+++ src/com/school/faang/hashset/задача_6/Solution.java
@@ -1,6 +1,13 @@
 package com.school.faang.hashset.задача_6;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Solution {
     public static void main(String[] args) {
