739a23d4e13e90b9a48d8d060c9b01cf7b980f0b
quikkkk <kexitttttt@gmail.com>
2025-06-24 16:18:22 +0300

    Решение задачи - 4

--- src/com/school/faang/hashset/задача_4/Solution.java
+++ src/com/school/faang/hashset/задача_4/Solution.java
@@ -1,5 +1,10 @@
 package com.school.faang.hashset.задача_4;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Solution {
     public static void main(String[] args) {
         ChannelManager manager = new ChannelManager();
