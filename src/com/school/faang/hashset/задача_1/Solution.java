d63009cf820605b1029a8d9ca3fb0ba2b8870dc8
Kate <119519512+Youta-Joy@users.noreply.github.com>
2025-06-24 16:06:46 +0300

    Решение задачи 1

--- src/com/school/faang/hashset/задача_1/Solution.java
+++ src/com/school/faang/hashset/задача_1/Solution.java
@@ -1,7 +1,41 @@
 package com.school.faang.hashset.задача_1;
 
+import java.util.HashSet;
+
 public class Solution {
     public static void main(String[] args) {
+        EventRegistration registration = new EventRegistration();
 
+        registration.register("@qwerty");
+        registration.register("@qwerty");
+        registration.register("@solo");
+        registration.register("@team");
+
+        System.out.println(registration.getParticipantCount());
+        System.out.println(registration.isRegistered("@qwerty"));
+    }
+}
+
+class EventRegistration {
+    private HashSet<String> registeredEmails = new HashSet<>();
+
+    public void register(String email) {
+        if (registeredEmails.add(email)) {
+            System.out.println("Регистрация для " + email + " прошла успешно!");
+        }else {
+            System.out.println("Ошибка: пользователь с email " + email + " уже зарегистрирован.");
+        }
+    }
+
+    public int getParticipantCount() {
+        return registeredEmails.size();
+    }
+
+    public boolean isRegistered(String email) {
+        if (registeredEmails.contains(email)) {
+            return true;
+        }else {
+            return false;
+        }
     }
 }
