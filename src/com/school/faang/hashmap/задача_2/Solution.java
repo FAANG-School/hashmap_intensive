87e63e4f7dacc160f63375147ea19ab47e52458e
Igor Ejov <iezho@yandex.ru>
2025-06-30 23:38:44 +0300

    Решение задачи HashMap 2

--- src/com/school/faang/hashmap/задача_2/Solution.java
+++ src/com/school/faang/hashmap/задача_2/Solution.java
@@ -2,6 +2,18 @@ package com.school.faang.hashmap.задача_2;
 
 public class Solution {
     public static void main(String[] args) {
+        UserDatabase userDatabase = new UserDatabase();
 
+        User user1 = new User(1L, "Иван Иванов", "2023-01-15");
+        User user2 = new User(2L, "Петр Петров", "2023-02-20");
+        User user3 = new User(3L, "Мария Сидорова", "2023-03-10");
+        User user4 = new User(4L, "Анна Кузнецова", "2023-04-05");
+
+        userDatabase.addUser(user1);
+        userDatabase.addUser(user2);
+        userDatabase.addUser(user3);
+        userDatabase.addUser(user4);
+
+        userDatabase.printAllUsersFormatted();
     }
 }
